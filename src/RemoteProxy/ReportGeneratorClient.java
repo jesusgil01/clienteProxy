3f7a434befb57598a79c23243195e0e0923ec75f
jalbertoornelass <55779135+jalbertoornelass@users.noreply.github.com>
2019-09-27 10:53:46 -0700

    Agregando Ventanas

---
+++ src/RemoteProxy/ReportGeneratorClient.java
@@ -0,0 +1,23 @@
+package RemoteProxy;
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+
+public class ReportGeneratorClient {
+
+	public static void main(String[] args) {
+		new ReportGeneratorClient().generateReport();
+	}
+	
+	public void generateReport(){
+		try {
+                        
+                        Registry registry = LocateRegistry.getRegistry("localhost", 9000);
+			ReportGenerator reportGenerator = (ReportGenerator)registry.lookup("ServidorProxy");
+			//System.out.println(reportGenerator.generateDailyReport());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
